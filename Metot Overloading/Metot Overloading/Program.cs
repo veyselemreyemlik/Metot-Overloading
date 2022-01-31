da46e6e8a2a61cf4e7c1f329e513fa25ef919ca2
Veysel Emre Yemlik <emreyemlik1905@gmail.com>
2022-02-01 01:49:36 +0300

    Add files via upload

---
+++ Metot Overloading/Metot Overloading/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Metot_Overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Out parametre kullanımı
+            string sayi = "999";
+            bool sonuc = int.TryParse(sayi, out int outSayi);
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı..");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız..");
+            }
+
+            Metotlar instance = new Metotlar();
+            instance.Topla(2, 3, out int toplamSonuc);
+            Console.WriteLine(toplamSonuc);
+
+            // Metot Aşırı Yükleme-Overloading
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Veysel ", " Yemlik");
+
+             // Metot İmzası 
+             // metotAdi + Parametre Sayisi + parametre
+
+
+
+            
+        }
+    }
+    class Metotlar
+    {
+        public void Topla(int a , int b , out int toplam)
+        {
+            toplam = a + b;
+        }
+
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public void EkranaYazdir(string veri1 , string veri2)
+        {
+            Console.WriteLine(veri1 + veri2);
+        }
+
+    }
+}
